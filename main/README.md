21aa5844f2fd338df9d697e90b638a2a11ee0c20
Tochukwu Nwoko <nwokotochukwu@gmail.com>
2022-03-23 08:38:42 +0100

    Rename main dir to chaninlink, Add terraform bastion modules, and Add necessary documentation

--- main/README.md
+++
@@ -1,2 +0,0 @@
-# chainlink-k8s
-Amazon EKS for Chainlink using Terraform (and Ansible if we feel like it)
--- main/userdata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-yum update -y
-sudo mkfs -t xfs /dev/xvdb
-sudo mkdir /data
-sudo mount /dev/xvdb /data
-id=$(blkid|grep xvdb|cut -d " " -f 2|cut -d "=" -f 2)
-echo "UUID=${id:1:-1}  /data  xfs  defaults,nofail  0  2" >> /etc/fstab
-yum -y install iscsi-initiator-utils
-systemctl enable --now iscsid
